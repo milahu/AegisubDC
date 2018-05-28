milahu/AegisubDC

commit f616154e02a42a28bdb51af0c06a42d00a30785f

Add the standard OS X "Window" menu

src/menu.cpp
@@ -27,6 +27,7 @@
 #include "format.h"
 #include "libresrc/libresrc.h"
 #include "options.h"
+#include "utils.h"
 
 #include <libaegisub/cajun/reader.h>
 #include <libaegisub/hotkey.h>
@@ -170,6 +171,10 @@ public:
 	{
 	}
 
+	void SetContext(agi::Context *c) {
+		context = c;
+	}
+
 	int AddCommand(cmd::Command *co, wxMenu *parent, std::string const& text = "") {
 		return AddCommand(co, parent, text.empty() ? co->StrMenu(context) : _(to_wx(text)));
 	}
@@ -229,6 +234,8 @@ public:
 	}
 
 	void OnMenuOpen(wxMenuEvent &) {
+		if (!context)
+			return;
 		for (auto const& item : dynamic_items) UpdateItem(item);
 		for (auto item : mru) item->Update();
 	}
@@ -237,7 +244,7 @@ public:
 		// This also gets clicks on unrelated things such as the toolbar, so
 		// the window ID ranges really need to be unique
 		size_t id = static_cast<size_t>(evt.GetId() - MENU_ID_BASE);
-		if (id < items.size())
+		if (id < items.size() && context)
 			cmd::call(items[id], context);
 
 #ifdef __WXMAC__
@@ -335,6 +342,11 @@ void process_menu_item(wxMenu *parent, agi::Context *c, json::Object const& ele,
 
 	std::string submenu, recent, command, text, special;
 	read_entry(ele, "special", &special);
+
+#ifdef __WXMAC__
+	if (special == "window")
+		osx::make_windows_menu(parent);
+#endif
 
 	if (read_entry(ele, "submenu", &submenu) && read_entry(ele, "text", &text)) {
 		wxString tl_text = _(to_wx(text));
@@ -487,6 +499,21 @@ public:
 
 namespace menu {
 	void GetMenuBar(std::string const& name, wxFrame *window, agi::Context *c) {
+#ifdef __WXMAC__
+		auto bind_events = [&](CommandMenuBar *menu) {
+			window->Bind(wxEVT_ACTIVATE, [=](wxActivateEvent&) { menu->cm.SetContext(c); });
+			window->Bind(wxEVT_DESTROY, [=](wxWindowDestroyEvent&) {
+				if (!osx::activate_top_window_other_than(window))
+					menu->cm.SetContext(nullptr);
+			});
+		};
+
+		if (wxMenuBar *menu = wxMenuBar::MacGetCommonMenuBar()) {
+			bind_events(static_cast<CommandMenuBar *>(menu));
+			return;
+		}
+#endif
+
 		auto menu = agi::make_unique<CommandMenuBar>(c);
 		for (auto const& item : get_menu(name)) {
 			std::string submenu, disp;
@@ -502,9 +529,15 @@ namespace menu {
 			}
 		}
 
-		window->Bind(wxEVT_MENU_OPEN, &CommandManager::OnMenuOpen, &menu->cm);
-		window->Bind(wxEVT_MENU, &CommandManager::OnMenuClick, &menu->cm);
+		menu->Bind(wxEVT_MENU_OPEN, &CommandManager::OnMenuOpen, &menu->cm);
+		menu->Bind(wxEVT_MENU, &CommandManager::OnMenuClick, &menu->cm);
+
+#ifdef __WXMAC__
+		bind_events(menu.get());
+		wxMenuBar::MacSetCommonMenuBar(menu.get());
+#else
 		window->SetMenuBar(menu.get());
+#endif
 
 		menu.release();
 	}

src/command/app.cpp
@@ -265,6 +265,41 @@ struct app_updates final : public Command {
 	}
 };
 
+#ifdef __WXMAC__
+struct app_minimize final : public Command {
+	CMD_NAME("app/minimize")
+	STR_MENU("Minimize")
+	STR_DISP("Minimize")
+	STR_HELP("Minimize the active window")
+
+	void operator()(agi::Context *c) override {
+		c->frame->Iconize();
+	}
+};
+
+struct app_maximize final : public Command {
+	CMD_NAME("app/maximize")
+	STR_MENU("Zoom")
+	STR_DISP("Zoom")
+	STR_HELP("Maximize the active window")
+
+	void operator()(agi::Context *c) override {
+		c->frame->Maximize(!c->frame->IsMaximized());
+	}
+};
+
+struct app_bring_to_front final : public Command {
+	CMD_NAME("app/bring_to_front")
+	STR_MENU("Bring All to Front")
+	STR_DISP("Bring All to Front")
+	STR_HELP("Bring forward all open documents to the front")
+
+	void operator()(agi::Context *) override {
+		osx::bring_to_front();
+	}
+};
+#endif
+
 }
 
 namespace cmd {
@@ -281,6 +316,11 @@ namespace cmd {
 		reg(agi::make_unique<app_options>());
 		reg(agi::make_unique<app_toggle_global_hotkeys>());
 		reg(agi::make_unique<app_toggle_toolbar>());
+#ifdef __WXMAC__
+		reg(agi::make_unique<app_minimize>());
+		reg(agi::make_unique<app_maximize>());
+		reg(agi::make_unique<app_bring_to_front>());
+#endif
 #ifdef WITH_UPDATE_CHECKER
 		reg(agi::make_unique<app_updates>());
 #endif

src/hotkey.cpp
@@ -47,6 +47,11 @@ namespace {
 		{nullptr}
 	};
 
+	const char *added_hotkeys_minimize[][3] = {
+		{"app/minimize", "Default", "Ctrl-M"},
+		{nullptr}
+	};
+
 	void migrate_hotkeys(const char *added[][3]) {
 		auto hk_map = hotkey::inst->GetHotkeyMap();
 		bool changed = false;
@@ -108,6 +113,13 @@ void init() {
 		hotkey::inst->SetHotkeyMap(std::move(hk_map));
 		migrations.emplace_back("duplicate -> split");
 	}
+
+#ifdef __WXMAC__
+	if (boost::find(migrations, "app/minimize") == end(migrations)) {
+		migrate_hotkeys(added_hotkeys_minimize);
+		migrations.emplace_back("app/minimize");
+	}
+#endif
 
 	OPT_SET("App/Hotkey Migrations")->SetListString(std::move(migrations));
 }

src/osx/osx_utils.mm
@@ -66,3 +66,25 @@ void RestartAegisub() {
         [NSTask launchedTaskWithLaunchPath:helperPath
                                  arguments:@[NSBundle.mainBundle.executablePath]];
 }
+
+namespace osx {
+void make_windows_menu(wxMenu* wxmenu) {
+	NSApp.windowsMenu = wxmenu->GetHMenu();
+}
+
+bool activate_top_window_other_than(wxFrame *wx) {
+	NSWindow *w = wx->GetWXWindow();
+	for (NSNumber *windowNumber in [NSWindow windowNumbersWithOptions:0]) {
+		NSWindow *window = [NSApp windowWithWindowNumber:windowNumber.integerValue];
+		if (window && window.canBecomeMainWindow && window != w) {
+			[window makeMainWindow];
+			return true;
+		}
+	}
+	return false;
+}
+
+void bring_to_front() {
+	[NSApp arrangeInFront:nil];
+}
+}

src/utils.h
@@ -99,11 +99,20 @@ agi::fs::path SaveFileSelector(wxString const& message, std::string const& optio
 
 wxString LocalizedLanguageName(wxString const& lang);
 
-namespace osx { namespace ime {
+namespace osx {
+	/// Make the given menu the OS X Window menu
+	void make_windows_menu(wxMenu *wxmenu);
+	/// Activate a top-level document window other than the given one
+	bool activate_top_window_other_than(wxFrame *wx);
+	// Bring all windows to the front, maintaining relative z-order
+	void bring_to_front();
+
+namespace ime {
 	/// Inject the IME helper into the given wxSTC
 	void inject(wxStyledTextCtrl *ctrl);
 	/// Invalidate any pending text from the IME
 	void invalidate(wxStyledTextCtrl *ctrl);
 	/// Give the IME a chance to process a key event and return whether it did
 	bool process_key_event(wxStyledTextCtrl *ctrl, wxKeyEvent &);
-} }
+}
+}

commit c4f2e84d980c93119280ed4cfbd868c7a10a0314

Fix MRU menus (#48)
Broken in f616154e02a42a28bdb51af0c06a42d00a30785f. I don't understand
why the author made such a change, so I left their solution with #ifdef
guard.

src/menu.cpp
@@ -419,7 +419,7 @@ class AutomationMenu final : public wxMenu {
 		WorkItem *FindOrMakeSubitem(std::string const &name) {
 			auto sub = std::find_if(subitems.begin(), subitems.end(), [&](WorkItem const &item) { return item.displayname == name; });
 			if (sub != subitems.end()) return &*sub;
-			
+
 			subitems.emplace_back(name);
 			return &subitems.back();
 		}
@@ -529,8 +529,13 @@ namespace menu {
 			}
 		}
 
+#ifdef __WXMAC__
 		menu->Bind(wxEVT_MENU_OPEN, &CommandManager::OnMenuOpen, &menu->cm);
 		menu->Bind(wxEVT_MENU, &CommandManager::OnMenuClick, &menu->cm);
+#else
+		window->Bind(wxEVT_MENU_OPEN, &CommandManager::OnMenuOpen, &menu->cm);
+		window->Bind(wxEVT_MENU, &CommandManager::OnMenuClick, &menu->cm);
+#endif
 
 #ifdef __WXMAC__
 		bind_events(menu.get());