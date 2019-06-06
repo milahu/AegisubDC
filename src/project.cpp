milahu/AegisubDC

commit 08eaa8fe1597cab7b2ffe73e4faf54b2f2658caf

Try auto loading audio from video only when actually loading a video
If the user drop some files into Aegisub, we try to load sub, video,
audio, etc from them. When "Automatically open audio when opening
video" is checked in config, we try to load audio from the path of
the video. This should only be done if the video is newly loaded. So
we should never try to load audio from video if the user is just
opening a subtitle.

See https://github.com/Aegisub/Aegisub/issues/121#issuecomment-498489847

src/project.cpp
@@ -524,7 +524,7 @@ void Project::LoadList(std::vector<agi::fs::path> const& files) {
 
 	if (!audio.empty())
 		DoLoadAudio(audio, false);
-	else if (OPT_GET("Video/Open Audio")->GetBool() && audio_file != video_file)
+	else if (!video.empty() && OPT_GET("Video/Open Audio")->GetBool() && audio_file != video_file)
 		DoLoadAudio(video_file, true);
 
 	if (!subs.empty())

commit 39b6d4658a9246ad8e64f196ce4ec77882d00f8e

Load audio when video is successfully loaded
Fix bug introduced in 08eaa8fe1597cab7b2ffe73e4faf54b2f2658caf

src/project.cpp
@@ -505,6 +505,9 @@ void Project::LoadList(std::vector<agi::fs::path> const& files) {
 			subs.clear();
 	}
 
+	if (!audio.empty())
+		DoLoadAudio(audio, false);
+
 	if (!video.empty() && DoLoadVideo(video)) {
 		double dar = video_provider->GetDAR();
 		if (dar > 0)
@@ -520,12 +523,11 @@ void Project::LoadList(std::vector<agi::fs::path> const& files) {
 			LoadTimecodes(timecodes);
 		if (!keyframes.empty())
 			LoadKeyframes(keyframes);
-	}
 
-	if (!audio.empty())
-		DoLoadAudio(audio, false);
-	else if (!video.empty() && OPT_GET("Video/Open Audio")->GetBool() && audio_file != video_file)
-		DoLoadAudio(video_file, true);
+		// Load audio from video
+		if (audio.empty() && OPT_GET("Video/Open Audio")->GetBool() && audio_file != video_file)
+			DoLoadAudio(video_file, true);
+	}
 
 	if (!subs.empty())
 		LoadUnloadFiles(properties);